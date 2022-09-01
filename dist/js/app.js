770d775144469fe951d1eb08eacd10dac89019eb
Nikita <nsotnikov16@yandex.ru>
2022-09-01 14:36:15 +0500

    update

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,6 +12,40 @@ testWebP(function (support) {
     document.documentElement.classList.add(className);
 });
 
+window.addEventListener("DOMContentLoaded", function () {
+    [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+        var keyCode;
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
+            var pos = this.selectionStart;
+            if (pos < 3) event.preventDefault();
+            var matrix = "+7 (___) ___-__-__",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, ""),
+                new_value = matrix.replace(/[_\d]/g, function (a) {
+                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+                });
+            i = new_value.indexOf("_");
+            if (i != -1) {
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i)
+            }
+            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                function (a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+            if (event.type == "blur" && this.value.length < 5) this.value = ""
+        }
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false)
+    });
+});
+
 /* Шапка */
 const header = document.querySelector('.header')
 const headerContactCopy = header.querySelector('.header__contact').cloneNode(true)
@@ -24,19 +58,14 @@ Array.from([headerMenuCopy, headerContactCopy]).forEach(element => {
 burger.addEventListener('click', () => header.classList.toggle('open'))
 
 
+
 /* Форма */
 const form = document.querySelector('.form')
 if (form) {
-    const phone = form.querySelector('#phone')
-    let maskPhone
-    if (phone) {
-        let im = new Inputmask("+7 (999)-999-99-99", { showMaskOnHover: false, alias: 'phonebe' });
-        maskPhone = im.mask(phone)
-    }
-
     const inputs = form.querySelectorAll('input')
     const textareas = form.querySelectorAll('textarea')
     Array.from([...inputs, ...textareas]).forEach(element => {
+        const isTel = element.classList.contains('tel')
         const parent = element.parentNode
         const error = parent.querySelector('.form__error')
         const label = element.nextElementSibling
@@ -46,6 +75,7 @@ if (form) {
             const message = element.validationMessage
             if (parent.classList.contains('form__row_file')) return;
 
+            if(isTel && element.value == '+7') element.value = ''
 
             if (element.value) {
                 label.style.display = 'none'
@@ -55,29 +85,27 @@ if (form) {
                 element.classList.remove('no-empty')
             }
 
-            if (element != phone) {
-                if (!element.validity.valid && error) {
-                    if (message && element.value) {
-                        error.textContent = message
-                    } else {
-                        error.textContent = error.dataset.text
-                    }
-                    error.style.display = 'block'
-                    element.classList.add('invalid')
+
+            if (!element.validity.valid && error) {
+                if (message && element.value) {
+                    error.textContent = message
                 } else {
-                    if (error) error.style.display = 'none'
-                    element.classList.remove('invalid')
+                    error.textContent = error.dataset.text
                 }
-            } else if (element == phone) {
-                if (!maskPhone.isComplete() && error) {
-                    error.style.display = 'block'
-                    element.classList.add('invalid')
-                } else {
-                    if (error) error.style.display = 'none'
-                    element.classList.remove('invalid')
-                }
+                error.style.display = 'block'
+                element.classList.add('invalid')
+            } else {
+                if (error) error.style.display = 'none'
+                element.classList.remove('invalid')
             }
 
+            if (isTel && element.value.length < 18 && error) {
+                error.style.display = 'block'
+                element.classList.add('invalid')
+            } else if (isTel) {
+                if (error) error.style.display = 'none'
+                element.classList.remove('invalid')
+            }
 
         })
     })
